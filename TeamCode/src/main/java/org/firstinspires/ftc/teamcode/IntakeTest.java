7abb612e3dd78ab498d920c690701c7dbdc7d55c
Nick <nick.koko.robotics@gmail.com>
2025-09-23 09:42:54 -0400

    Update to new version of Panels with the new FTC field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -14,12 +15,13 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
  * - Uses RUN_USING_ENCODER velocity control with DcMotorEx
  * - Left bumper spins forward, Right bumper spins backward
  */
+@Configurable
 @TeleOp(name = "Intake Test", group = "Test")
 public class IntakeTest extends LinearOpMode {
 
-    private static final double M1_TARGET_RPM = 435.0; // top front
-    private static final double M2_TARGET_RPM = 350.0; // bottom
-    private static final double M3_TARGET_RPM = 300.0; // top back
+    public static double M1_TARGET_RPM = 435.0; // top front
+    public static double M2_TARGET_RPM = 350.0; // bottom
+    public static double M3_TARGET_RPM = 300.0; // top back
 
     // goBILDA Yellow Jacket integrated encoder has 28 ticks per motor revolution
     private static final double ENCODER_TICKS_PER_MOTOR_REV = 28.0;
@@ -44,7 +46,7 @@ public class IntakeTest extends LinearOpMode {
         m3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         m1.setDirection(DcMotorSimple.Direction.FORWARD);
         m2.setDirection(DcMotorSimple.Direction.REVERSE);
-        m2.setDirection(DcMotorSimple.Direction.FORWARD);
+        m3.setDirection(DcMotorSimple.Direction.FORWARD);
 
         telemetry.addLine("Intake Test ready. LB=forward, RB=reverse");
         telemetry.addData("m1 target RPM", M1_TARGET_RPM);
@@ -59,10 +61,6 @@ public class IntakeTest extends LinearOpMode {
         final double m1TicksPerRev = ENCODER_TICKS_PER_MOTOR_REV * M1_GEAR_RATIO; // 28 * 3.7 = 103.6
         final double m2TicksPerRev = ENCODER_TICKS_PER_MOTOR_REV * M2_GEAR_RATIO; // 28 * 13.7 = 383.6
         final double m3TicksPerRev = ENCODER_TICKS_PER_MOTOR_REV * M3_GEAR_RATIO; // 28 * 13.7 = 383.6
-
-        final double m1TicksPerSec = rpmToTicksPerSecond(M1_TARGET_RPM, m1TicksPerRev);
-        final double m2TicksPerSec = rpmToTicksPerSecond(M2_TARGET_RPM, m2TicksPerRev);
-        final double m3TicksPerSec = rpmToTicksPerSecond(M3_TARGET_RPM, m3TicksPerRev);
 
         boolean m1Enabled = true;
         boolean m2Enabled = true;
@@ -84,6 +82,11 @@ public class IntakeTest extends LinearOpMode {
             if (y && !prevY) m2Enabled = !m2Enabled;
             if (b && !prevB) m3Enabled = !m3Enabled;
 
+            // Recompute velocities from current (possibly updated) target RPMs
+            final double m1TicksPerSec = rpmToTicksPerSecond(M1_TARGET_RPM, m1TicksPerRev);
+            final double m2TicksPerSec = rpmToTicksPerSecond(M2_TARGET_RPM, m2TicksPerRev);
+            final double m3TicksPerSec = rpmToTicksPerSecond(M3_TARGET_RPM, m3TicksPerRev);
+
             if (forward ^ reverse) { // exactly one pressed
                 double direction = forward ? 1.0 : -1.0;
                 if (m1Enabled) m1.setVelocity(direction * m1TicksPerSec); else m1.setPower(0.0);
@@ -96,10 +99,16 @@ public class IntakeTest extends LinearOpMode {
                 m3.setPower(0.0);
             }
 
+            // Compute current RPMs from measured ticks/sec and gear-adjusted ticks/rev
+            double m1Rpm = m1.getVelocity() * 60.0 / m1TicksPerRev;
+            double m2Rpm = m2.getVelocity() * 60.0 / m2TicksPerRev;
+            double m3Rpm = m3.getVelocity() * 60.0 / m3TicksPerRev;
+
             telemetry.addData("Mode", forward ? "Forward" : (reverse ? "Reverse" : "Stopped"));
-            telemetry.addData("m1", "%s  vel=%.1f tps", m1Enabled ? "EN" : "OFF", m1.getVelocity());
-            telemetry.addData("m2", "%s  vel=%.1f tps", m2Enabled ? "EN" : "OFF", m2.getVelocity());
-            telemetry.addData("m3", "%s  vel=%.1f tps", m3Enabled ? "EN" : "OFF", m3.getVelocity());
+            telemetry.addData("targets rpm", "m1=%.0f m2=%.0f m3=%.0f", M1_TARGET_RPM, M2_TARGET_RPM, M3_TARGET_RPM);
+            telemetry.addData("m1", "%s  rpm=%.1f", m1Enabled ? "EN" : "OFF", m1Rpm);
+            telemetry.addData("m2", "%s  rpm=%.1f", m2Enabled ? "EN" : "OFF", m2Rpm);
+            telemetry.addData("m3", "%s  rpm=%.1f", m3Enabled ? "EN" : "OFF", m3Rpm);
             telemetry.update();
 
             prevX = x;
